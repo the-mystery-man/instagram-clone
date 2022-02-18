02e8fc3543a745b2dc6fc55350255c4c815534ea
Slenzy2 <tobiikupolati33@gmail.com>
2022-02-18 13:14:19 +0100

    T

--- src/components/Suggestion.vue
+++ src/components/Suggestion.vue
@@ -47,51 +47,11 @@
 
         </div>
         <!-- <Tooltip :toolTip = "toolTip" /> -->
-        <q-btn>
-            Hello Boss
-
-            <q-tooltip v-on:mouseover="open" class="tooltip" anchor="bottom end" self="center middle">
-              This thing should please work abeg.
-              <q-btn v-on="on">
-                Hello world
-              </q-btn>
-            </q-tooltip>
-
-        </q-btn>
 
 
 
 
-            <q-btn color="primary" label="Basic Menu">
-              <q-menu>
-                <q-list >
-                  <q-item clickable v-close-popup>
-                    <q-item-section>New tab</q-item-section>
-                  </q-item>
-                  <q-item clickable v-close-popup>
-                    <q-item-section>New incognito tab</q-item-section>
-                  </q-item>
-                  <q-separator />
-                  <q-item clickable v-close-popup>
-                    <q-item-section>Recent tabs</q-item-section>
-                  </q-item>
-                  <q-item clickable v-close-popup>
-                    <q-item-section>History</q-item-section>
-                  </q-item>
-                  <q-item clickable v-close-popup>
-                    <q-item-section>Downloads</q-item-section>
-                  </q-item>
-                  <q-separator />
-                  <q-item clickable v-close-popup>
-                    <q-item-section>Settings</q-item-section>
-                  </q-item>
-                  <q-separator />
-                  <q-item clickable v-close-popup>
-                    <q-item-section>Help &amp; Feedback</q-item-section>
-                  </q-item>
-                </q-list>
-              </q-menu>
-            </q-btn>
+
 
 
 </template>
